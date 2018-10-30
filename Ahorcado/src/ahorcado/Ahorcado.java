d49ddfcb3acc3e589d2efb33fa57bad3b5830d93
alumno <alumno@aula1-02>
2018-10-30 18:13:22 +0100

    ultimo codigo antes de que corrija el profesor

--- Ahorcado/src/ahorcado/Ahorcado.java
+++ Ahorcado/src/ahorcado/Ahorcado.java
@@ -15,39 +15,84 @@ public class Ahorcado {
 
     
        
-    //para el contador de intentos
+    //te pide que digas la palabra. para el contador de intentos
     public static int gameOVER(){
    
         int intentos;
         intentos=7;
             
-         System.out.println("[=====================]");
-            System.out.println("");                    
-            //mostrar el vector de la palabra
-            System.out.println("");
-            System.out.println("Dime una letra");
-            String letra;
-            Scanner leer2 = new Scanner(System.in);
-            letra = leer2.next();
-            System.out.println("[=====================]");
+        System.out.println("[=====================]");
+        System.out.println("");                    
+        //mostrar el vector de la palabra
+        System.out.println("");
+        System.out.println("Dime una letra");
+        String letra;
+        Scanner leer2 = new Scanner(System.in);
+        letra = leer2.next();
+        System.out.println("[=====================]");               
         
-        
+ 
+
         return intentos;
     }
     
+    //poner mi palabra en el vector
+    
+  public static void inicializarVectorpalabra(String[] vector){
+        
+        String palabra;
+        
+        palabra="marcos";
+               
+        for (int i = 0; i < vector.length; i++) {
    
+            vector[i]=palabra.substring(i, palabra.length()-1);
+         
+        }
+        
+     
+        
+    }
+  
+  //poner el vector de sacar palabra con guiones
+  
+    public static void inicializarVectorsacar(String[] vector){
+       
+                  
+        for (int i = 0; i < vector.length; i++) {
+   
+            vector[i]="-";
+         
+        }
+        
+    }
+    //comprobar la letra que escribas con la palabra
+    
+    public static void comprobarVectorpalabra(String[] vector, String letra){
+        
+        //como mando al un vector que esta en una funcion y una variavle que esta en otra funcion
+        
+    }
+      
     
     
     public static void main(String[] args) {
      
         //varibales principales
-          int empezarjuego;
-   
+        int empezarjuego;
+          
+        //inicializar el vector de la palabra  
+        String[] vPalabra = new String[6];
+        inicializarVectorpalabra(vPalabra);
+        
+        //inicializar el vector de la palabra a adivinar 
+        String[] vSacar = new String[6];
+        inicializarVectorsacar(vSacar);
         
     do{
     
         
-          System.out.println("[=====================]");
+        System.out.println("[=====================]");
         System.out.println("|                     |");
         System.out.println("|     AhorcadoGAME    |");
         System.out.println("|                     |");
